a390833f70d01fb65f1395619ff6b38d819fb4ca
Jumpei Yoshimra <jumpyoshim@gmail.com>
2019-03-04 16:32:35 +0000

    Add Blog Update API #9

--- blogs/factories.py
+++ blogs/factories.py
@@ -1,4 +1,3 @@
-import factory
 from factory.django import DjangoModelFactory
 
 from .models import Blog
@@ -7,10 +6,3 @@ from .models import Blog
 class BlogFactory(DjangoModelFactory):
     class Meta:
         model = Blog
-
-    title = factory.Faker('title')
-    url = factory.Faker('url')
-    thumbnail = factory.Faker('thumbnail')
-    start_dt = factory.Faker('start_dt')
-    end_dt = factory.Faker('end_dt')
-    is_public = factory.Faker('is_public')
